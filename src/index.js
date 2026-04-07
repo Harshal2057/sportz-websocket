f7a5a00ef7394b30c2bc074a993f5cd0308e477a
Harshal2057 <harshcbhandarkar@gmail.com>
2026-04-08 03:14:52 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.get("/" , (req , res) => {
+    res.send("Hello from Express")
+})
+
+app.listen(port , () => {
+    console.log(`The server is running on http://localhost:${port}`);
+    
+})
