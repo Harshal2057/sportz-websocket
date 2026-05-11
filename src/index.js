b40ac66ed060665145c68f6adc3476e88023250e
Harshal2057 <harshcbhandarkar@gmail.com>
2026-05-11 08:15:19 +0530

    ping pong

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import express from "express"
 import { MatchRouter } from "./routes/matches.js";
+import http from "http"
+import { attachWebsocketServer } from "./ws/server.js";
+import { log } from "console";
 
-const port = 8000;
+const PORT = Number(process.env.PORT || 8000);
+const HOST = process.env.HOST || "0.0.0.0"
 
 const app = express();
+const server = http.createServer(app);
+
 
 app.use(express.json());
 
@@ -13,6 +19,12 @@ app.get("/" , (req , res) => {
 
 app.use("/matches" , MatchRouter)
 
-app.listen(port , () => {
-  console.log(`The server is running on http://localhost:${port}`)
+const { broadcastMatchCreated } = attachWebsocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated
+
+app.listen(PORT , HOST , () => {
+  const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`
+
+  console.log(`The server is running on http://localhost:${PORT}`)
+  console.log(`Websocket server is running on ${baseUrl.replace('http' , 'ws')}/ws`)
 })
